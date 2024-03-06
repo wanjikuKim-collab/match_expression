4bb99aa1759c907eb9484b74fa0bd993edbca8e0
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-06 11:15:06 +0300

    created Coin and UsState enum

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+enum Coin{
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState{
+    Alabama,
+    Alaska,
+    Arizona,
+    Arkansas
+}
+
+
+
+fn main() {
+
+}
+
