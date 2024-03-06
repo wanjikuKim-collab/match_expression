f422b52706ff5da65d6ef379913c5404c68c5050
wanjikuKim-collab <wanjikukimani24@gmail.com>
2024-03-06 11:15:59 +0300

    Used match expression to check value of cents

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,23 @@ enum UsState{
     Arkansas
 }
 
+fn value_in_cents(coin: Coin) -> u8{
+    match coin{
+        Coin::Penny=>{
+            println!("Lucky penny");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) =>{
+            println!("State quarter from {:?}",state);
+            25
+        },
 
+    }
+}
 
 fn main() {
-
+    
 }
 
